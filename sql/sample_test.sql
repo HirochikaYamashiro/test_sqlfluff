0c5a412920cfe027691822ad5ef7c1c63e4f38d7
Hirochika Yamashiro <hirochika.yamashiro@rizapgroup.com>
2023-03-09 18:37:03 +0900

    update test

--- sql/sample_test.sql
+++ sql/sample_test.sql
@@ -1,4 +1,5 @@
-with load_ec_order as (
+with 
+load_ec_order as (
     select distinct
         order_id
         , customer_id
@@ -15,92 +16,98 @@ with load_ec_order as (
 )
 
 , join_customers as (
-  SELECT 
-    ec_order.total_price
-    , cast(extract(month from ec_order.created_at) as string) as month
-    , cast(extract(year from ec_order.created_at) as string) as year
-    , case gender
-        when 1 then '男性'
-        when 2 then '女性' 
-        else 'その他'
-      end as gender
-    , IF(round_age >= 70, '70以上', cast(round_age as string)) AS round_age
-  FROM 
-    load_ec_order as ec_order
-  INNER join (select email, customer_id from load_customer) ec_customer 
+    SELECT 
+        ec_order.total_price
+        , cast(extract(month from ec_order.created_at) as string) as month
+        , cast(extract(year from ec_order.created_at) as string) as year
+        , case gender
+            when 1 then '男性'
+            when 2 then '女性' 
+            else 'その他'
+            end as gender
+        , IF(round_age >= 70, '70以上', cast(round_age as string)) AS round_age
+    FROM 
+        load_ec_order as ec_order
+    INNER join (
+        select 
+            email
+            , customer_id 
+        from 
+            load_customer
+    ) as ec_customer 
     using(customer_id)
-  inner join (
-      select 
-        mail_address 
-        , gender
-        , round(FLOOR(DATE_DIFF(CURRENT_DATE('Asia/Tokyo'), birthday, DAY)/3650))*10 as round_age
-      from `hgym-340203.hacomono_chocozap.会員`
+    inner join (
+        select 
+            mail_address 
+            , gender
+            , round(FLOOR(DATE_DIFF(CURRENT_DATE('Asia/Tokyo'), birthday, DAY)/3650))*10 as round_age
+        from `hgym-340203.hacomono_chocozap.会員`
     ) as choco_customer
     on ec_customer.email = choco_customer.mail_address
 )
 
 , calc_price as (
-  select 
-    year
-    , month
-    , gender
-    , round_age
-    , sum(total_price) as price
-    , count(total_price) as num_order
-  from join_customers
-  where gender != 'その他'
-  group by year, month, gender, round_age
+    select 
+        year
+        , month
+        , gender
+        , round_age
+        , sum(total_price) as price
+        , count(total_price) as num_order
+    from join_customers
+    where gender != 'その他'
+    group by year, month, gender, round_age
 )
 
 , calc_gender_total as (
-  select 
-    year
-    , month
-    , '小計' as gender
-    , round_age
-    , sum(total_price) as price
-    , count(total_price) as num_order
-  from join_customers
-  where gender != 'その他'
-  group by year, month, round_age
+    select 
+        year
+        , month
+        , '小計' as gender
+        , round_age
+        , sum(total_price) as price
+        , count(total_price) as num_order
+    from join_customers
+    where gender != 'その他'
+    group by year, month, round_age
 )
 
 , calc_age_total as (
-  select 
-    year
-    , month
-    , gender
-    , '小計' as round_age
-    , sum(total_price) as price
-    , count(total_price) as num_order
-  from join_customers
-  where gender != 'その他'
-  group by year, month, gender
+    select 
+        year
+        , month
+        , gender
+        , '小計' as round_age
+        , sum(total_price) as price
+        , count(total_price) as num_order
+    from join_customers
+    where gender != 'その他'
+    group by year, month, gender
 )
 
 , calc_month_total as (
-  select '小計' as year
-    , '小計' as month
-    , gender
-    , round_age
-    , sum(total_price) as price
-    , count(total_price) as num_order
-  from join_customers
-  where gender != 'その他'
-  group by gender, round_age
+    select '小計' as year
+        , '小計' as month
+        , gender
+        , round_age
+        , sum(total_price) as price
+        , count(total_price) as num_order
+    from join_customers
+    where gender != 'その他'
+    group by gender, round_age
 )
 
 , calc_month_age_total as (
-  select
-    '小計' year
-    , '小計' as month
-    , gender
-    , '小計' as round_age
-    , sum(total_price) as price
-    , count(total_price) as num_order
-  from join_customers
-  where gender != 'その他'
-  group by gender
+    select
+        '小計' year
+        , '小計' as month
+        , gender
+        , '小計' as round_age
+        , sum(total_price) as price
+        , count(total_price) as num_order
+    from join_customers
+    where gender != 'その他'
+    group by gender
 )
 
 -- choco ECの売り上げを元に年代、性別でそれぞれいくら(金額)注文したのか集計してほしい
